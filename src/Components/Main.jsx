93ea0ca9edcc2efd668fe82afc6fcd8b470888d5
Sarvarjon Namazov <namazovsarvar@gmail.com>
2024-03-12 07:21:49 -0700

    Space api

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,21 +2,30 @@
 import { useState, useEffect } from "react"
 import NewsItem from "./NewsItem"
 
+// https://api.thenewsapi.com/v1/news/all?api_token=CeJ6bHVGiNF2oQN9znURzBFAfodfQS1KIxYoV5xc&language=en&limit=3
+// let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+
 export default function Main({ category }) {
     const [article, setArticle] = useState([])
 
     useEffect(() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
-        fetch(url).then(response => response.json()).then(data => setArticle(data.articles))
-    }, [category])
+        let url = `https://api.spaceflightnewsapi.net/v4/articles/?limit=10`
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setArticle(data.results))
+            .catch(error => console.log('Error fetching articles:', error))
+    }, [])
 
 
     return (
         <div className="container">
             <h2 className="text-center">Latest <span className="badge bg-primary">News</span></h2>
+
             {article.map((news, index) => {
-                return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+                return <NewsItem key={index} title={news.title} description={news.summary} src={news.image_url} url={news.url} />
             })}
+
         </div>
+
     )
 }
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -3,8 +3,10 @@ import image from '../assets/news.jpg'
 
 export default function NewsItem({ title, description, src, url }) {
     return (
-        <div className="card bg-dark text-light d-inline-block m-3" style={{ width: '400px' }}>
-            <img src={src ? src : image} style={{ height: '250px', width: '100%' }} />
+        <div className="card bg-dark text-light d-inline-block m-3" style={{ maxWidth: '400px' }}>
+
+            <img src={src ? src : image} className='card-img-top' style={{ height: '250px', width: '100%' }} />
+
             <div className="card-body">
                 <h5 className="card-title">{title.slice(0, 20)}</h5>
                 <p className="card-text">{description ? description.slice(0, 90) : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum consectetur nemo commodi.'}</p>
